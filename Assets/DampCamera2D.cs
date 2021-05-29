2f11ca3b332d838d910b6b2d1bdf0a1478a7acbd
JulioFerrero <cf19julio.ferrero@iesjoandaustria.org>
2021-05-29 14:54:15 +0200

    📷 Better camera

--- Assets/DampCamera2D.cs
+++ Assets/DampCamera2D.cs
@@ -7,11 +7,25 @@ using UnityEngine;
          public Transform target;
          public float smoothTime = 0.3F;
          private Vector3 velocity = Vector3.zero;
+         private Vector3 targetPosition;
+
+         void Start()
+         {
+             targetPosition = target.TransformPoint(new Vector3(0.8f, 0.16f, -1));
+         }
 
          void Update()
          {
              // Define a target position above and behind the target transform
-             Vector3 targetPosition = target.TransformPoint(new Vector3(0.8f, 0.16f, -1));
+             
+             if (Input.GetKey("left"))
+             {
+                 targetPosition = target.TransformPoint(new Vector3(-0.8f, 0.16f, -1));
+             }
+             else if (Input.GetKey("right"))
+             {
+                 targetPosition = target.TransformPoint(new Vector3(0.8f, 0.16f, -1));
+             }
 
              // Smoothly move the camera towards that target position
              transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
