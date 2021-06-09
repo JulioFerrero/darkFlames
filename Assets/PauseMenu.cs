d62b3cc52c14f18853322c5fd48265d677bc19e9
JulioFerrero <cf19julio.ferrero@iesjoandaustria.org>
2021-06-10 00:36:32 +0200

    ⏯️ New menu options (resolution, fullscreen and volume).

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,31 +1,52 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool isPaused;
     public GameObject pauseMenu;
 
-    public static bool isPaused;
+    public TMP_Dropdown resolutionDropdown;
+
+    public AudioMixer audioMixer;
+
+    private Resolution[] resolutions;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         pauseMenu.SetActive(false);
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        var options = new List<string>();
+
+        var currentResolutionIndex = 0;
+        for (var i = 0; i < resolutions.Length; i++)
+        {
+            var option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height) currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
-            {
                 ResumeGame();
-            }
             else
-            {
                 PauseGame();
-            }
         }
     }
 
@@ -47,5 +68,20 @@ public class PauseMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
- 
