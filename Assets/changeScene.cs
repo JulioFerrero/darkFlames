40856b85b298417a46b1c608f58540bcd9ba16f4
JulioFerrero <cf19julio.ferrero@iesjoandaustria.org>
2021-06-10 03:20:29 +0200

    🖼️ Added start scene.

---
+++ Assets/changeScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeScene : MonoBehaviour
+{
+    public void loadlevel()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+ 
+    }
+}
